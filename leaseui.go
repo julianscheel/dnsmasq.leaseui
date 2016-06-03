4c4f510d375d8208fb00dd306a12232e79540b43
Julian Scheel <julian@jusst.de>
2016-06-03 15:48:28 +0200

    Add oui database backend to lookup vendors
    Read oui database from local file oui.txt if available. Otherwise load it from
    standards.ieee.net. Additionally the database is updated weekly in background.
    
    Signed-off-by: Julian Scheel <julian@jusst.de>

--- leaseui.go
+++ leaseui.go
@@ -2,27 +2,31 @@ package main
 
 import (
 	"encoding/csv"
+	"io/ioutil"
+	"bytes"
 	"fmt"
 	"os"
 	"strconv"
 	"github.com/kataras/iris"
+	"github.com/klauspost/oui"
+	"github.com/jasonlvhit/gocron"
 )
 
 type Lease struct {
 	Expiry int
 	Mac string
+	MacVendor string
 	Ip string
 	Hostname string
 	ClientId string
 }
 
-func parseLeases() []Lease {
+func parseLeases(ouiDb oui.DynamicDB) []Lease {
 	csvFile, err := os.Open("/var/lib/misc/dnsmasq.leases")
 	if err != nil {
 		fmt.Println(err)
 		return nil
 	}
-
 	defer csvFile.Close()
 
 	reader := csv.NewReader(csvFile)
@@ -61,11 +65,36 @@ type page struct {
 	Title string
 }
 
+func updateOuiDb(db oui.DynamicDB) {
+	fmt.Println("Start oui database update...")
+	oui.UpdateHttp(db, "http://standards-oui.ieee.org/oui.txt")
+	fmt.Println("Done.")
+}
+
 func main() {
 	iris.Config().Render.Template.Directory = "./templates/web/default"
+	db, err := oui.OpenFile("oui.txt")
+	if err != nil {
+		/* No local cache exists, create empty database */
+		io := ioutil.NopCloser(bytes.NewReader(nil))
+		db, err = oui.Open(io)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	/* Start oui update in background */
+	go updateOuiDb(db)
+
+	/* Schedule weekly auto update */
+	gocron.Every(1).Day().Do(updateOuiDb, 1, db)
+	_, time := gocron.NextRun()
+	fmt.Println(time)
+	gocron.Start()
 
 	iris.Get("/leases", func(c *iris.Context) {
-		c.JSON(200, iris.Map{ "data": parseLeases() } )
+		c.JSON(200, iris.Map{ "data": parseLeases(db) } )
 	})
 	iris.Static("/css", "./static/css", 1)
 	iris.Static("/js", "./static/js", 1)
